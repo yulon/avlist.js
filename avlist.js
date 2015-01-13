cc8507b4d6670d1a6f4fbf498dc0f5a579948f78
Yuchen Wang <yulon@me.com>
2015-01-14 05:15:25 +0800

    减少闭�

--- avlist.js
+++ avlist.js
@@ -6,77 +6,77 @@ HTMLElement.prototype.avList = function(name){
 };
 
 function HTMLElementAttributeValueList (e, n) {
-
-	this.contains = function(){
-		var value = e.getAttribute(n);
-		if (value != null) {
-			var valueList = value.split(/\s+/);
-			var c = 0;
-
-			for (var i = 0; i < arguments.length; i++) {
-				for (var y = 0; y < valueList.length; y++) {
-					if (arguments[i] == valueList[y]) {
-						c++;
-						break;
-					};
-				};
-			};
-
-			return c == arguments.length;
-		};
-		return false;
-	};
-
-	this.add = function(){
-		var value = e.getAttribute(n);
-		var newValue;
-		if (value) {
-			var valueList = value.split(/\s+/);
-			newValue = value;
-			for (var i = 0; i < arguments.length; i++) {
-				var exist = false;
-				for (var y = 0; y < valueList.length; y++) {
-					if (arguments[i] == valueList[y]) {
-						exist = true;
-						break;
-					};
-				};
-				if (exist == false) {
-					newValue += " " + arguments[i];
-				};
-			};
-		}else{
-			newValue = "";
-			for (var i = 0; i < arguments.length; i++) {
-				newValue += arguments[i] + " ";
-			};
-			newValue = newValue.slice(0, -1);
-		};
-		e.setAttribute(n, newValue);
-	};
-
-	this.remove = function(){
-		var value = e.getAttribute(n);
-		if (value) {
-			var valueList = value.split(/\s+/);
-			var newValue;
-
-			for (var i = 0; i < valueList.length; i++) {
-				var exist = false;
-				for (var y = 0; y < arguments.length; y++) {
-					if (arguments[y] == valueList[i]) {
-						exist = true;
-						break;
-					};
-				};
-				if (exist == false) {
-					newValue += valueList[i] + " ";
-				};
-			};
-
-			e.setAttribute(n, newValue.slice(0, -1));
-		};
-		
-	};
-
+	this.element = e;
+	this.attrName = n;
 }
+
+HTMLElementAttributeValueList.prototype.contains = function() {
+	var value = this.element.getAttribute(this.attrName);
+	if (value != null) {
+		var valueList = value.split(/\s+/);
+		var c = 0;
+
+		for (var i = 0; i < arguments.length; i++) {
+			for (var y = 0; y < valueList.length; y++) {
+				if (arguments[i] == valueList[y]) {
+					c++;
+					break;
+				};
+			};
+		};
+
+		return c == arguments.length;
+	};
+	return false;
+};
+
+HTMLElementAttributeValueList.prototype.add = function(){
+	var value = this.element.getAttribute(this.attrName);
+	var newValue;
+	if (value) {
+		var valueList = value.split(/\s+/);
+		newValue = value;
+		for (var i = 0; i < arguments.length; i++) {
+			var exist = false;
+			for (var y = 0; y < valueList.length; y++) {
+				if (arguments[i] == valueList[y]) {
+					exist = true;
+					break;
+				};
+			};
+			if (exist == false) {
+				newValue += " " + arguments[i];
+			};
+		};
+	}else{
+		newValue = "";
+		for (var i = 0; i < arguments.length; i++) {
+			newValue += arguments[i] + " ";
+		};
+		newValue = newValue.slice(0, -1);
+	};
+	this.element.setAttribute(this.attrName, newValue);
+};
+
+HTMLElementAttributeValueList.prototype.remove = function(){
+	var value = this.element.getAttribute(this.attrName);
+	if (value) {
+		var valueList = value.split(/\s+/);
+		var newValue;
+
+		for (var i = 0; i < valueList.length; i++) {
+			var exist = false;
+			for (var y = 0; y < arguments.length; y++) {
+				if (arguments[y] == valueList[i]) {
+					exist = true;
+					break;
+				};
+			};
+			if (exist == false) {
+				newValue += valueList[i] + " ";
+			};
+		};
+
+		this.element.setAttribute(this.attrName, newValue.slice(0, -1));
+	};
+};
