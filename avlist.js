6b957a97c4cb9b46af7a033f28c31e3abf23daec
Yuchen Wang <yulon@me.com>
2015-01-14 05:04:58 +0800

    修正属性不存在时报错 & 增加 demo

--- avlist.js
+++ avlist.js
@@ -9,61 +9,74 @@ function HTMLElementAttributeValueList (e, n) {
 
 	this.contains = function(){
 		var value = e.getAttribute(n);
-		var valueList = value.split(/\s+/);
-		var c = 0;
+		if (value != null) {
+			var valueList = value.split(/\s+/);
+			var c = 0;
 
-		for (var i = 0; i < arguments.length; i++) {
-			for (var y = 0; y < valueList.length; y++) {
-				if (arguments[i] == valueList[y]) {
-					c++;
-					break;
+			for (var i = 0; i < arguments.length; i++) {
+				for (var y = 0; y < valueList.length; y++) {
+					if (arguments[i] == valueList[y]) {
+						c++;
+						break;
+					};
 				};
 			};
-		};
 
-		return c == arguments.length;
+			return c == arguments.length;
+		};
+		return false;
 	};
 
 	this.add = function(){
 		var value = e.getAttribute(n);
-		var valueList = value.split(/\s+/);
-		var addValue;
-
-		for (var i = 0; i < arguments.length; i++) {
-			var exist = false;
-			for (var y = 0; y < valueList.length; y++) {
-				if (arguments[i] == valueList[y]) {
-					exist = true;
-					break;
+		var newValue;
+		if (value) {
+			var valueList = value.split(/\s+/);
+			newValue = value;
+			for (var i = 0; i < arguments.length; i++) {
+				var exist = false;
+				for (var y = 0; y < valueList.length; y++) {
+					if (arguments[i] == valueList[y]) {
+						exist = true;
+						break;
+					};
+				};
+				if (exist == false) {
+					newValue += " " + arguments[i];
 				};
 			};
-			if (exist == false) {
-				addValue += " " + arguments[i];
+		}else{
+			newValue = "";
+			for (var i = 0; i < arguments.length; i++) {
+				newValue += arguments[i] + " ";
 			};
+			newValue = newValue.slice(0, -1);
 		};
-
-		e.setAttribute(n, value + addValue);
+		e.setAttribute(n, newValue);
 	};
 
 	this.remove = function(){
 		var value = e.getAttribute(n);
-		var valueList = value.split(/\s+/);
-		var newValue;
+		if (value) {
+			var valueList = value.split(/\s+/);
+			var newValue;
 
-		for (var i = 0; i < valueList.length; i++) {
-			var exist = false;
-			for (var y = 0; y < arguments.length; y++) {
-				if (arguments[y] == valueList[i]) {
-					exist = true;
-					break;
+			for (var i = 0; i < valueList.length; i++) {
+				var exist = false;
+				for (var y = 0; y < arguments.length; y++) {
+					if (arguments[y] == valueList[i]) {
+						exist = true;
+						break;
+					};
+				};
+				if (exist == false) {
+					newValue += valueList[i] + " ";
 				};
 			};
-			if (exist == false) {
-				newValue += valueList[i] + " ";
-			};
-		};
 
-		e.setAttribute(n, newValue.slice(0, -2));
+			e.setAttribute(n, newValue.slice(0, -1));
+		};
+		
 	};
 
 }
